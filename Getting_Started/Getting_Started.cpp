f146fc07b5f8254475f120587dcb569094f6ec76
Patel Vishwas <149381035+destroyer246k@users.noreply.github.com>
2025-07-09 19:46:00 +0530

    Update Getting_Started.cpp

--- Getting_Started/Getting_Started.cpp
+++ Getting_Started/Getting_Started.cpp
@@ -1,9 +1,11 @@
 #include<iostream>// This line includes the iostream library which is used for input and output in C++. 
-using namespace std; // The 'using namespace std;' line allows us to use standard library features without prefixing them with 'std::'.
+using namespace std; // The 'using namespace std;' line allows us to use standard library features without prefixing them with std::.
 
-int main(){
+int main()
+{
+    //NOTE: See arrows used in output(cout<<) and input(cin>>) carrfully
     std::cout << "Welcome to C++ Programming!" << endl; // if std was not defined in line 2.
-    cout << "Hello World" << endl; //endline(endl) is used to print a new line
+    cout << "Hello World" << endl; //endline(endl) is used to print a new line.
     int a; // Declare an integer variable 'a' to store user input.
     cout << "Enter a number: ";
     cin >> a;  
