f0b7f668996606d2b4231a522b52e504cf265807
Patel Vishwas <149381035+destroyer246k@users.noreply.github.com>
2025-07-13 09:41:21 +0530

    Update Exp1_Calculator.cpp

--- Exp1_Calculator/Exp1_Calculator.cpp
+++ Exp1_Calculator/Exp1_Calculator.cpp
@@ -1,13 +1,19 @@
 #include<iostream> // This line includes the iostream library which is used for input and output in C++.
 using namespace std; // This line allows us to use standard library features without prefixing them with std::.
 int main(){
+
     //NOTE: See arrows used in output(cout<<) and input(cin>>) carefully.
+
     std::cout << "Welcome to C++ Programming!" << endl; // if std was not defined in line 2.
+
     cout << "Hello World" << endl; //endline(endl) is used to print a new line.
-    int a; // Declare an integer variable 'a' to store user input.
+
+    int x; // Declare an integer variable 'x' to store user input.
     cout << "Enter a number: ";
-    cin >> a;  
-    cout << "You entered: " << a << endl;
+    cin >> x;  // Read an integer from user input and store it in 'x'.
+    cout << "You entered: " << x << endl;
+
+    // Calculator Program
     float a, b, sum, sub, mul, div; // Declare variables for two numbers and their results.
     cout << "Enter number 1: "; // Prompt user for first number
     cin >> a; // Read first number from user input
@@ -22,4 +28,5 @@ int main(){
     cout << "Multiplication: " << mul << endl;
     cout << "Division: " << div << endl;
     return 0;
+    
 }
